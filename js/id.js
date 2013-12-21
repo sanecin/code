3ecec489d9564f498ede5bf7f2a0deff05d67dae
SanEcIn <sanecin@gmail.com>
2013-12-21 22:44:45 +0300

    ?

---
+++ js/id.js
@@ -0,0 +1,23 @@
+//[¤[mdx][https://mdx.googlecode.com/git/id.js]@[vers_1.0_¤]¤¤¤ ...
+
+//======== Id ===========
+// ... element ID query ... speed ... element[ID="f"] => Id.f ... !q ...
+
+(window.Id=function(){
+
+ 	'use strict';
+
+	var i,j
+// ... for next ... window.onload only ...
+	var _=function (){for(i=(j=document.querySelectorAll('[id]')).length;i--;Id[j[i].id]=j[i]){}}
+
+// ... toGO ... common
+	if (window.addEventListener) window.addEventListener("load",_, false); else window.onload=_;
+
+// ... in case ... event.js REQUIRED ...
+//	if (typeof(Event.On)==="function") ... ???
+//		Event.On(window,"load",Fit._);
+
+})();
+
+//<!-- /* [Copyright 11/12/2012 SanEcIn][¤¤] */-->
