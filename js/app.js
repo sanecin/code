c5e0e5a9bc37b1dbc1f2f1be6305c88cab57e387
SanEcIn <sanecin@gmail.com>
2014-01-22 23:51:45 +0300

    app.js

--- js/app.js
+++ js/app.js
@@ -3,30 +3,28 @@
 //======== App ===========
 // ... local namespace ... :/ ...
 
-isF=function(k){return(typeof k==="function")}
-isS=function(k){return(typeof k==="string")}
+isF=function(k){return "function"===typeof k}
+isS=function(k){return "string"===typeof k}
 
-//(function (){
+(function (){
 
-// 	'use strict';
-var App={
+ 	'use strict';
+	var App=window.App={
 
-	otl:0,//1//0// ... App_debug ...
+		otl:0,//1//0// ... App_debug ...
 
-	vers:"1.0",
+		vers:"1.0",
 
-	_:function(k){
+		_:function(k){
 
-		if(isF(k)) return k;
+			if(isF(k)) return k;
 
-		if(typeof k==="string") if(typeof window[k]==="function") return window[k];
+			if(isS(k)&isF(window[k])) return window[k];
+
+		}
 
 	}
 
-}
-
-//	window.App=App;
-
-//}());
+}());
 
 //<!-- /* [Copyright 11/12/2012 SanEcIn][¤¤] */-->
