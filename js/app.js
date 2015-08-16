4a19210c18fe1dbbeafe042c4d08749589a743e3
SanEcIn <sanecin@gmail.com>
2015-08-16 16:59:43 +0300

    .

---
+++ js/app.js
@@ -0,0 +1,31 @@
+//[¤[mdx][https://lib.mdx.googlecode.com/git/JS/app.js]@[vers_1.0_¤]¤¤¤ ...
+
+//======== App ===========
+// ... local namespace ? ... :/ ...
+
+isF=function(k){return "function"===typeof k}
+isS=function(k){return "string"===typeof k}
+
+	var App={
+
+		otl:0,//1//0// ... App_debug ...
+
+		vers:"1.0",
+
+		_:function(k){
+
+			if(isF(k)) return k;
+
+			if(isS(k)&isF(window[k])) return window[k];
+
+			if (Lib._(k)) return k;
+
+// ...
+
+			return false;
+
+		}
+
+	}
+
+//<!-- /* [Copyright 11/12/2012 SanEcIn][¤¤] */-->
